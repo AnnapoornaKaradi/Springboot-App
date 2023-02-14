5f165c262e0c4906ca248203482b9aafa583e14d
Annu <annapoornakaradi96@gmail.com>
2023-02-15 01:22:58 +0530

    added files

---
+++ src/main/java/com/example/Springbootdemo/HelloDemo.java
@@ -0,0 +1,15 @@
+package com.example.Springbootdemo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rest")
+public class HelloDemo {
+
+	@GetMapping("/get")
+	public String sayHello() {
+		return "Hello ! Welcome to Spring boot application";
+
+	}
+
+}
